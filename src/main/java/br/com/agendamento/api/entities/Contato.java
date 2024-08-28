7381424d96db417051400c61de3513fe87cef512
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-08-28 10:32:54 -0300

    Add: flyway migrations

--- src/main/java/br/com/agendamento/api/entities/Contato.java
+++ src/main/java/br/com/agendamento/api/entities/Contato.java
@@ -2,6 +2,7 @@ package br.com.agendamento.api.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,8 +17,8 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Contato {
     @Id
-    @GeneratedValue
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String email;
     private String celular;
